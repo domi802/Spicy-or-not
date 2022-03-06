c0bf8bd0f797db9aaeeb7374f4a936b002a95641
domi802 <64065747+domi802@users.noreply.github.com>
2022-03-06 21:50:29 +0100

    Initial Commit

---
+++ Hello-Binding/Hello_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_BindingApp.swift
+//  Hello-Binding
+//
+//  Created by Dominik Woźniak on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hello_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
