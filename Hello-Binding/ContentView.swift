9d7e3bbd75831b7b25e65fa49e2dfdaa55fdedb6
domi802 <64065747+domi802@users.noreply.github.com>
2022-04-28 00:19:58 +0200

    ADD

--- Hello-Binding/ContentView.swift
+++ Hello-Binding/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var model = Dish.all()
+    @State private var isSpicy = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        List{
+            
+            Toggle(isOn: $isSpicy) {
+                Text("Spicy")
+                    .font(.title)
+            }
+            
+            
+            ForEach(model.filter{ $0.isSpicy == self.isSpicy}) {dish in
+                
+                HStack {
+                    Image(dish.imageURL)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    
+                    Text(dish.name)
+                        .font(.title)
+                        .lineLimit(nil)
+                    
+                    if(dish.isSpicy){
+                    
+                    Image("spicy-ikon")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
+        
     }
 }
 
---
+++ Hello-Binding/Models/Dish.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+import SwiftUI
+
+struct Dish: Identifiable{
+    let id = UUID()
+    let name: String
+    let imageURL: String
+    let isSpicy: Bool
+}
+
+extension Dish{
+    
+    static func all() -> [Dish]{
+        
+        return[
+        Dish(name: "King POw Chicken", imageURL: "kungpow", isSpicy:true),
+        Dish(name: "Sweet and Sour Chicken", imageURL: "sweet", isSpicy: false),
+        Dish(name: "Spicy Red Chicken", imageURL: "spicy", isSpicy: true)
+        
+        ]
+    }
+    
+}
